1c5872c1d10df5558ae391fca8f9028bd56aab39
Akinwande Olubodun <oleue_mana@yahoo.com>
2025-05-20 03:35:42 +0100

    added files

--- script.js
+++ script.js
@@ -6,7 +6,8 @@
 //To review, an array is an ordered collection of data where each item is assigned
 //an index starting from 0
 //e.g.
-let mySampleArray = ["Index 0", "Index 1", "Index 2"];
+let mySampleArray = [
+  "Index 0", "Index 1", "Index 2"];
 
 //An object, is used to store collections of data in a key-value pair format,
 //where each key is a string (although quotes are optional for keys without spaces in them) and
@@ -35,8 +36,8 @@ let mySampleObject = {
 
 const myZooAnimals = [
   { animalType: 'Feline', name: "Lion", age: 12, pictureURL:"https://media.istockphoto.com/id/685049686/photo/lioness-with-cubs.jpg?s=1024x1024&w=is&k=20&c=cDspEM_wAuX0h5PnZrCleyk0jL6-Xw1MTVUCFYzVDk4="},
-  { animalType: 'Avian', name: "Ostrich", age: 10, pictureURL:"https://images.unsplash.com/photo-1580145654058-6571bfa12a95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=998&q=80"},
-  { animalType: 'Apes', name: "Baboon", age: 15, pictureURL:"https://images.unsplash.com/photo-1589307817967-c612f7daa06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1494&q=80"},
+  { animalType: 'Avian', name: "Ostrich", age: 10, pictureURL:"https://images.unsplash.com/photo-1589307817967-c612f7daa06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1494&q=80"},
+  { animalType: 'Apes', name: "Baboon", age: 15, pictureURL:"https://images.unsplash.com/photo-1580145654058-6571bfa12a95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=998&q=80"},
 ];
 
 
